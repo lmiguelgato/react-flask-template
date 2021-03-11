cbb86094f5da50d522768ed9b689ec22d72622ac
Luis M. Gato <lmiguelgato@gmail.com>
2021-03-10 18:15:16 -0600

    Update App component, add button to start long task.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
+import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap'
-import IconLabelButtons from './components/Button'
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Awesome web app using React and Flask.</NavbarBrand>
-        </div>
-      </Navbar>
-      <IconLabelButtons />
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false
+    };
+  }
+
+  handlePredictClick = (event) => {
+    this.setState({ isLoading: true });
+    fetch('http://127.0.0.1:5000/longtask', 
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({"elementid": "1", "userid": "2"})
+      })
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          isLoading: false
+        });
+      });
+  }
+
+  render() {
+    const isLoading = this.state.isLoading;
+
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Awesome web app using React and Flask.</NavbarBrand>
+          </div>
+        </Navbar>
+        <Button
+          block
+          variant="success"
+          disabled={isLoading}
+          onClick={!isLoading ? this.handlePredictClick : null}>
+          { isLoading ? 'Making prediction' : 'Predict' }
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
