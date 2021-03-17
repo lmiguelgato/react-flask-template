d862fe37c55ea1107c97b7f2c2df974bdda6bbb3
Luis M. Gato <lmiguelgato@gmail.com>
2021-03-16 18:45:57 -0600

    Replace class component with functional component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { io } from "socket.io-client";
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Button from 'react-bootstrap/Button';
@@ -6,35 +6,27 @@ import './App.css';
 
 
 const socket = io("http://localhost:5000/events");
-class App extends Component {
+function App() {
 
-  constructor(props) {
-    super(props);
+  const [isLoading, setIsLoading] = useState(false);
+  const [userId, setUserId] = useState("");
+  
+  console.log(socket)
+  socket.on("connect", message => {
+    console.log(message);
+    socket.emit('status', {status: 'I\'m connected!'});
+  })
+  socket.on("userid", message => {
+    console.log(message);
+    setUserId(message.userid)
+  })
+  socket.on("celerystatus", data => {
+    console.log(data);
+  });
 
-    this.state = {
-      isLoading: false,
-      userId: ""
-    };
-    
-    console.log(socket)
-    socket.on("connect", message => {
-      console.log(message);
-      socket.emit('status', {status: 'I\'m connected!'});
-    })
-    socket.on("userid", message => {
-      console.log(message);
-      this.setState({
-        userId: message.userid
-      })
-    })
-    socket.on("celerystatus", data => {
-      console.log(data);
-    });
-  }
-
-  handlePredictClick = (event) => {
-    this.setState({ isLoading: true });
-    console.log('test')
+  const handlePredictClick = (event) => {
+    setIsLoading(true);
+    console.log('test');
     fetch('http://localhost:5000/longtask', 
       {
         headers: {
@@ -42,40 +34,30 @@ class App extends Component {
           'Content-Type': 'application/json'
         },
         method: 'POST',
-        body: JSON.stringify({"elementid": "1", "userid": "2"})
+        body: JSON.stringify({"elementid": "1", "userid": userId})
       })
       .then(response => response.json())
-      .then(response => {
-        this.setState({
-          isLoading: false
-        });
-      });
+      .then(
+        () => setIsLoading(false)
+      );
   }
 
-  componentDidMount() {
-    
-  }
-
-  render() {
-    const isLoading = this.state.isLoading;
-
-    return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Awesome web app using React and Flask.</NavbarBrand>
-          </div>
-        </Navbar>
-        <Button
-          block
-          variant="success"
-          disabled={isLoading}
-          onClick={!isLoading ? this.handlePredictClick : null}>
-          { isLoading ? 'Making prediction' : 'Predict' }
-        </Button>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Awesome web app using React and Flask.</NavbarBrand>
+        </div>
+      </Navbar>
+      <Button
+        block
+        variant="success"
+        disabled={isLoading}
+        onClick={!isLoading ? handlePredictClick : null}>
+        { isLoading ? 'Making prediction' : 'Predict' }
+      </Button>
+    </div>
+  );
 }
 
 export default App;
