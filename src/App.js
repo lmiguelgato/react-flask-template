ac57088b7b4f9a09c05f6a29f501d00d99319dbf
Luis M. Gato <lmiguelgato@gmail.com>
2021-03-30 13:49:01 -0600

    Add Websocket component

--- src/App.js
+++ src/App.js
@@ -1,61 +1,19 @@
-import React, { useState } from 'react';
-import { io } from "socket.io-client";
 import { Navbar, NavbarBrand } from 'reactstrap';
-import Button from 'react-bootstrap/Button';
 import './App.css';
+import Websocket from './components/Websocket';
 
 
-const socket = io("http://localhost:5000/events");
 function App() {
-
-  const [isLoading, setIsLoading] = useState(false);
-  const [userId, setUserId] = useState("");
-  
-  console.log(socket)
-  socket.on("connect", message => {
-    console.log(message);
-    socket.emit('status', {status: 'I\'m connected!'});
-  })
-  socket.on("userid", message => {
-    console.log(message);
-    setUserId(message.userid)
-  })
-  socket.on("celerystatus", data => {
-    console.log(data);
-  });
-
-  const handlePredictClick = (event) => {
-    setIsLoading(true);
-    console.log('test');
-    fetch('http://localhost:5000/longtask', 
-      {
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        method: 'POST',
-        body: JSON.stringify({"elementid": "1", "userid": userId})
-      })
-      .then(response => response.json())
-      .then(
-        () => setIsLoading(false)
-      );
-  }
-
   return (
     <div className="App">
       <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Awesome web app using React and Flask.</NavbarBrand>
-        </div>
+      <div className="container">
+        <NavbarBrand href="/">Awesome web app using React and Flask.</NavbarBrand>
+      </div>
       </Navbar>
-      <Button
-        block
-        variant="success"
-        disabled={isLoading}
-        onClick={!isLoading ? handlePredictClick : null}>
-        { isLoading ? 'Making prediction' : 'Predict' }
-      </Button>
+      <header className="App-header">
+        <Websocket />
+      </header>
     </div>
   );
 }
---
+++ src/components/Websocket.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import axios from 'axios'
+import socketIOClient from "socket.io-client";
+
+const ENDPOINT = 'http://127.0.0.1:5000'
+
+function Websocket() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [userId, setUserId] = useState('unassigned');
+  const [statusList, setStatusList] = useState([])
+
+  const startJob = async () => {
+    setIsLoading(true)
+    console.log('Starting job')
+    const data = await axios.post('http://localhost:5000/job', {user_id: userId})
+    setStatusList([])
+  }
+
+  useEffect(() => {
+    console.log('IN USE EFFECT');
+    const socket = socketIOClient(ENDPOINT);
+
+    socket.on('connected', data => {
+      console.log('Event', data);
+      setUserId(data['user_id'])
+    });
+
+    socket.on('status', data => {
+      console.log('Status', data);
+      console.log(statusList)
+      setStatusList(oldStatusList => [...oldStatusList, data.key])
+    });
+  }, [])
+
+  return (
+    <div>
+      <span>User ID: {userId}</span> <br />
+      <Button
+      block
+      variant="success"
+      disabled={isLoading}
+      onClick={!isLoading ? startJob : null}>
+      { isLoading ? 'Making prediction' : 'Predict' }
+      </Button>
+      {statusList.map((status, index) => (<p key={index}>{status}</p>))}
+    </div>
+  )
+}
+
+export default Websocket;
