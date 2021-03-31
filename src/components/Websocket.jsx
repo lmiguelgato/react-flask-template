3bcf1f930c13e63073d3693da26103f33cd1225f
Luis M. Gato <miguel.gato@bairesdev.com>
2021-03-31 16:18:47 -0600

    Refactor

--- src/components/Websocket.jsx
+++ src/components/Websocket.jsx
@@ -1,35 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import axios from 'axios'
-import socketIOClient from "socket.io-client";
+import axios from 'axios';
+import socketIOClient from 'socket.io-client';
 
-const ENDPOINT = 'http://127.0.0.1:5000'
+const ENDPOINT = 'http://127.0.0.1:5000';
 
 function Websocket() {
   const [isLoading, setIsLoading] = useState(false);
-  const [userId, setUserId] = useState('unassigned');
-  const [status, setStatus] = useState('')
-  const [progress, setProgress] = useState(0)
+  const [userId, setUserId] = useState('');
+  const [status, setStatus] = useState('');
+  const [progress, setProgress] = useState(0);
 
   const startJob = async () => {
-    setIsLoading(true)
-    const data = await axios.post('http://localhost:5000/job', {user_id: userId})
-    setStatus(data.data.status)
+    setIsLoading(true);
+    
+    const api_response = await axios.post(
+      'http://localhost:5000/job',
+      {user_id: userId}
+    );
+    
+    setStatus(api_response.data.status);
   }
 
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
-    socket.on('connected', data => {
-      setUserId(data['user_id'])
-    });
+    
+    socket.on('connected', data => setUserId(data['user_id']));
 
     socket.on('status', data => {
       if (data.current === data.total) {
-        setIsLoading(false)
-        setProgress(0)
+        setIsLoading(false);
+        setProgress(0);
       } else {
-        setProgress(Math.floor(100*data.current/data.total))
-        setStatus(data.status)
+        setProgress(Math.floor(100*data.current/data.total));
+        setStatus(data.status);
       }
     });
   }, [])
@@ -37,12 +41,13 @@ function Websocket() {
   return (
     <div>
       <Button
-      block
       variant="success"
+      size="lg"
       disabled={isLoading}
       onClick={!isLoading ? startJob : null}>
-      { isLoading ? 'Making prediction (' + progress + ' %)' : 'Predict' }
+      { isLoading ? 'Training model (' + progress + ' %)' : 'Click to train' }
       </Button>
+      <br />
       {status}
     </div>
   )
